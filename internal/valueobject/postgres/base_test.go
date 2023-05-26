97f5af6220ef19923c8a17e2ce7b14182e13976a
Braejan Arias <ing.brayanarias@gmail.com>
2023-05-26 16:10:27 -0500

    :white_check_mark: test previous handlers implementations

--- internal/valueobject/postgres/base_test.go
+++ internal/valueobject/postgres/base_test.go
@@ -1,1 +1,219 @@
 package postgres_test
+
+import (
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/braejan/go-transactions-summary/internal/valueobject/postgres"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestNewBasePostgresDatabase tests the NewBasePostgresDatabase function.
+func TestNewBasePostgresDatabase(t *testing.T) {
+	//When call NewBasePostgresDatabase
+	postgresRepo := postgres.NewBasePostgresDatabase()
+	// Then return a new instance of PostgresDatabase interface implementation.
+	assert.NotNil(t, postgresRepo)
+}
+
+// TestCloseFail tests the Close function fails.
+func TestCloseFail(t *testing.T) {
+	// Given a mock database
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+	// And a mock database that fails
+	mock.ExpectClose().WillReturnError(assert.AnError)
+	// When call Close
+	err = postgres.NewBasePostgresDatabase().Close(db)
+	// Then return an error
+	assert.Error(t, err)
+}
+
+// TestCloseSuccess tests the Close function succeeds.
+func TestCloseSuccess(t *testing.T) {
+	// Given a mock database
+	db, dbMock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+	// And a mock database that succeeds
+	dbMock.ExpectClose().WillReturnError(nil)
+	// When call Close
+	err = postgres.NewBasePostgresDatabase().Close(db)
+	// Then return no error
+	assert.NoError(t, err)
+}
+
+// TestBeginTxFail tests the BeginTx function fails.
+func TestBeginTxFail(t *testing.T) {
+	// Given a mock database
+	db, dbMock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+	// And a mock database that fails
+	dbMock.ExpectBegin().WillReturnError(assert.AnError)
+	// When call BeginTx
+	tx, err := postgres.NewBasePostgresDatabase().BeginTx(db)
+	// Then return an error
+	assert.Error(t, err)
+	assert.Nil(t, tx)
+}
+
+// TestBeginTxSuccess tests the BeginTx function succeeds.
+func TestBeginTxSuccess(t *testing.T) {
+	// Given a mock database
+	db, dbMock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+	// And a mock database that succeeds
+	dbMock.ExpectBegin().WillReturnError(nil)
+	// When call BeginTx
+	tx, err := postgres.NewBasePostgresDatabase().BeginTx(db)
+	// Then return no error
+	assert.NoError(t, err)
+	assert.NotNil(t, tx)
+}
+
+// TestCommitFail tests the Commit function fails.
+func TestCommitFail(t *testing.T) {
+	// Given a mock database
+	db, dbMock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+	// And a mock database that fails
+	dbMock.ExpectBegin().WillReturnError(nil)
+	dbMock.ExpectCommit().WillReturnError(assert.AnError)
+	// And a mocked dbTx
+	tx, err := db.Begin()
+	assert.NoError(t, err)
+	// When call Commit
+	err = postgres.NewBasePostgresDatabase().Commit(tx)
+	// Then return an error
+	assert.Error(t, err)
+}
+
+// TestCommitSuccess tests the Commit function succeeds.
+func TestCommitSuccess(t *testing.T) {
+	// Given a mock database
+	db, dbMock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+	// And a mock database that succeeds
+	dbMock.ExpectBegin().WillReturnError(nil)
+	dbMock.ExpectCommit().WillReturnError(nil)
+	// And a mocked dbTx
+	tx, err := db.Begin()
+	assert.NoError(t, err)
+	// When call Commit
+	err = postgres.NewBasePostgresDatabase().Commit(tx)
+	// Then return no error
+	assert.NoError(t, err)
+}
+
+// TestRollbackFail tests the Rollback function fails.
+func TestRollbackFail(t *testing.T) {
+	// Given a mock database
+	db, dbMock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+	// And a mock database that fails
+	dbMock.ExpectBegin().WillReturnError(nil)
+	dbMock.ExpectRollback().WillReturnError(assert.AnError)
+	// And a mocked dbTx
+	tx, err := db.Begin()
+	assert.NoError(t, err)
+	// When call Rollback
+	err = postgres.NewBasePostgresDatabase().Rollback(tx)
+	// Then return an error
+	assert.Error(t, err)
+}
+
+// TestRollbackSuccess tests the Rollback function succeeds.
+func TestRollbackSuccess(t *testing.T) {
+	// Given a mock database
+	db, dbMock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+	// And a mock database that succeeds
+	dbMock.ExpectBegin().WillReturnError(nil)
+	dbMock.ExpectRollback().WillReturnError(nil)
+	// And a mocked dbTx
+	tx, err := db.Begin()
+	assert.NoError(t, err)
+	// When call Rollback
+	err = postgres.NewBasePostgresDatabase().Rollback(tx)
+	// Then return no error
+	assert.NoError(t, err)
+}
+
+// TestExecFail tests the Exec function fails.
+func TestExecFail(t *testing.T) {
+	// Given a mock database
+	db, dbMock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+	// And a mock database that fails
+	dbMock.ExpectBegin().WillReturnError(nil)
+	dbMock.ExpectExec("test").WillReturnError(assert.AnError)
+	// And a mocked dbTx
+	tx, err := db.Begin()
+	assert.NoError(t, err)
+	// When call Exec
+	_, err = postgres.NewBasePostgresDatabase().Exec(tx, "test")
+	// Then return an error
+	assert.Error(t, err)
+}
+
+// TestExecSuccess tests the Exec function succeeds.
+func TestExecSuccess(t *testing.T) {
+	// Given a mock database
+	db, dbMock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+	// And a mock database that succeeds
+	dbMock.ExpectBegin().WillReturnError(nil)
+	dbMock.ExpectExec("test").WillReturnResult(sqlmock.NewResult(1, 1))
+	// And a mocked dbTx
+	tx, err := db.Begin()
+	assert.NoError(t, err)
+	// When call Exec
+	_, err = postgres.NewBasePostgresDatabase().Exec(tx, "test")
+	// Then return no error
+	assert.NoError(t, err)
+}
+
+// TestQueryFail tests the Query function fails.
+func TestQueryFail(t *testing.T) {
+	// Given a mock database
+	db, dbMock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+	// And a mock database that fails
+	dbMock.ExpectBegin().WillReturnError(nil)
+	dbMock.ExpectQuery("test").WillReturnError(assert.AnError)
+	// And a mocked dbTx
+	tx, err := db.Begin()
+	assert.NoError(t, err)
+	// When call Query
+	_, err = postgres.NewBasePostgresDatabase().Query(tx, "test")
+	// Then return an error
+	assert.Error(t, err)
+}
+
+// TestQuerySuccess tests the Query function succeeds.
+func TestQuerySuccess(t *testing.T) {
+	// Given a mock database
+	db, dbMock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+	// And a mock database that succeeds
+	dbMock.ExpectBegin().WillReturnError(nil)
+	dbMock.ExpectQuery("test").WillReturnRows(sqlmock.NewRows([]string{"test"}).AddRow("test"))
+	// And a mocked dbTx
+	tx, err := db.Begin()
+	assert.NoError(t, err)
+	// When call Query
+	_, err = postgres.NewBasePostgresDatabase().Query(tx, "test")
+	// Then return no error
+	assert.NoError(t, err)
+}
---
+++ internal/valueobject/postgres/postgres_test.go
@@ -0,0 +1,144 @@
+package postgres_test
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/braejan/go-transactions-summary/internal/valueobject/postgres"
+	"github.com/stretchr/testify/assert"
+)
+
+func resetEnvironmentPostgresVariables() {
+	os.Unsetenv("POSTGRES_HOST")
+	os.Unsetenv("POSTGRES_PORT")
+	os.Unsetenv("POSTGRES_USER")
+	os.Unsetenv("POSTGRES_PASSWORD")
+	os.Unsetenv("POSTGRES_DATABASE")
+}
+
+// TestNewPostgresConfigurationSuccess tests the NewPostgresConfiguration function succeeds.
+func TestNewPostgresConfigurationSuccess(t *testing.T) {
+	// Given a host
+	host := "test"
+	// And a port
+	port := 1234
+	// And a user
+	user := "test"
+	// And a password
+	password := "test"
+	// And a database
+	database := "test"
+	// When call NewPostgresConfiguration
+	configuration := postgres.NewPostgresConfiguration(host, port, user, password, database)
+	// Then return a PostgresConfiguration
+	assert.Equal(t, host, configuration.Host)
+	assert.Equal(t, port, configuration.Port)
+	assert.Equal(t, user, configuration.User)
+	assert.Equal(t, password, configuration.Password)
+	assert.Equal(t, database, configuration.Database)
+}
+
+// TestGetDefaultPostgresConfigurationSuccess tests the GetDefaultPostgresConfiguration function succeeds.
+func TestGetDefaultPostgresConfigurationSuccess(t *testing.T) {
+	// When call GetDefaultPostgresConfiguration
+	configuration := postgres.GetDefaultPostgresConfiguration()
+	log.Printf("datasource: %s\n", configuration.GetDataSourceName())
+	// Then return a PostgresConfiguration
+	assert.Equal(t, "localhost", configuration.Host)
+	assert.Equal(t, 5432, configuration.Port)
+	assert.Equal(t, "postgres", configuration.User)
+	assert.Equal(t, "postgres", configuration.Password)
+	assert.Equal(t, "stori-challenge-db", configuration.Database)
+}
+
+// TestGetPostgresConfigurationFromEnvSuccess tests the GetPostgresConfigurationFromEnv function succeeds.
+func TestGetPostgresConfigurationFromEnvSuccess(t *testing.T) {
+	// reset environment variables
+	resetEnvironmentPostgresVariables()
+	// Given a POSTGRES_HOST environment variable
+	err := os.Setenv("POSTGRES_HOST", "localhost")
+	assert.NoError(t, err)
+	// And a POSTGRES_PORT environment variable
+	err = os.Setenv("POSTGRES_PORT", "1234")
+	assert.NoError(t, err)
+	// And a POSTGRES_USER environment variable
+	err = os.Setenv("POSTGRES_USER", "postgres")
+	assert.NoError(t, err)
+	// And a POSTGRES_PASSWORD environment variable
+	err = os.Setenv("POSTGRES_PASSWORD", "postgres")
+	assert.NoError(t, err)
+	// And a POSTGRES_DATABASE environment variable
+	err = os.Setenv("POSTGRES_DATABASE", "stori-challenge-db")
+	assert.NoError(t, err)
+	// When call GetPostgresConfigurationFromEnv
+	configuration := postgres.GetPostgresConfigurationFromEnv()
+	// Then return a PostgresConfiguration
+	assert.Equal(t, "localhost", configuration.Host)
+	assert.Equal(t, 1234, configuration.Port)
+	assert.Equal(t, "postgres", configuration.User)
+	assert.Equal(t, "postgres", configuration.Password)
+	assert.Equal(t, "stori-challenge-db", configuration.Database)
+}
+
+// TestGetPostgresConfigurationFromEnvWithEmptyHostSuccess tests the GetPostgresConfigurationFromEnv function succeeds.
+func TestGetPostgresConfigurationFromEnvWithEmptyHostSuccess(t *testing.T) {
+	// reset environment variables
+	resetEnvironmentPostgresVariables()
+	// Given a POSTGRES_PORT environment variable
+	err := os.Setenv("POSTGRES_PORT", "1234")
+	assert.NoError(t, err)
+	// And a POSTGRES_USER environment variable
+	err = os.Setenv("POSTGRES_USER", "postgres")
+	assert.NoError(t, err)
+	// And a POSTGRES_PASSWORD environment variable
+	err = os.Setenv("POSTGRES_PASSWORD", "postgres")
+	assert.NoError(t, err)
+	// And a POSTGRES_DATABASE environment variable
+	err = os.Setenv("POSTGRES_DATABASE", "other-db")
+	assert.NoError(t, err)
+	// When call GetPostgresConfigurationFromEnv
+	configuration := postgres.GetPostgresConfigurationFromEnv()
+	// Then return a PostgresConfiguration
+	assert.Equal(t, "localhost", configuration.Host)
+	assert.Equal(t, 5432, configuration.Port)
+	assert.Equal(t, "postgres", configuration.User)
+	assert.Equal(t, "postgres", configuration.Password)
+	assert.Equal(t, "stori-challenge-db", configuration.Database)
+}
+
+// TestGetPostgresConfigurationFromEnvWithEmptyPortSuccess tests the GetPostgresConfigurationFromEnv function succeeds.
+func TestGetPostgresConfigurationFromEnvWithEmptyPortSuccess(t *testing.T) {
+	// reset environment variables
+	resetEnvironmentPostgresVariables()
+	// Given a POSTGRES_HOST environment variable
+	err := os.Setenv("POSTGRES_HOST", "localhost")
+	assert.NoError(t, err)
+	// And a POSTGRES_USER environment variable
+	err = os.Setenv("POSTGRES_USER", "postgres")
+	assert.NoError(t, err)
+	// And a POSTGRES_PASSWORD environment variable
+	err = os.Setenv("POSTGRES_PASSWORD", "postgres")
+	assert.NoError(t, err)
+	// And a POSTGRES_DATABASE environment variable
+	err = os.Setenv("POSTGRES_DATABASE", "other-db")
+	assert.NoError(t, err)
+	// When call GetPostgresConfigurationFromEnv
+	configuration := postgres.GetPostgresConfigurationFromEnv()
+	// Then return a PostgresConfiguration
+	assert.Equal(t, "localhost", configuration.Host)
+	assert.Equal(t, 5432, configuration.Port)
+	assert.Equal(t, "postgres", configuration.User)
+	assert.Equal(t, "postgres", configuration.Password)
+	assert.Equal(t, "stori-challenge-db", configuration.Database)
+}
+
+// TestGetDataSoureNameSuccess tests the GetDataSourceName function succeeds.
+func TestGetDataSoureNameSuccess(t *testing.T) {
+	// Given a PostgresConfiguration
+	configuration := postgres.GetDefaultPostgresConfiguration()
+	// When call GetDataSourceName
+	dataSourceName := configuration.GetDataSourceName()
+	// Then return a data source name
+	assert.Equal(t, "host=localhost port=5432 user=postgres password=postgres dbname=stori-challenge-db sslmode=disable", dataSourceName)
+}
