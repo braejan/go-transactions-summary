121959d74f9004fcf4fe346d7ff3f0636e2b29bf
Braejan Arias <ing.brayanarias@gmail.com>
2023-05-25 20:50:59 -0500

    :white_check_mark: transactions tests passing.

--- internal/domain/transaction/repository/postgres/transaction_test.go
+++ internal/domain/transaction/repository/postgres/transaction_test.go
@@ -1,1 +1,20 @@
 package postgres_test
+
+import (
+	"testing"
+
+	"github.com/braejan/go-transactions-summary/internal/domain/transaction/repository/postgres"
+	voPostgres "github.com/braejan/go-transactions-summary/internal/valueobject/postgres"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestNewPostgresTransactionRepositoryWithNilConfig tests the NewPostgresTransactionRepository function
+// when the configuration is nil.
+func TestNewPostgresTransactionRepositoryWithNilConfig(t *testing.T) {
+	// Given a nil configuration.
+	// When NewPostgresTransactionRepository is called.
+	_, err := postgres.NewPostgresTransactionRepository(nil, nil)
+	// Then the error returned should be ErrNilConfiguration.
+	assert.NotNil(t, err)
+	assert.Equal(t, voPostgres.ErrNilConfiguration, err)
+}
